afb4ed59ae8bb5700bf4911149168e2c7db7d8d6
sarthakm21 <sarthakmaheshwari140@gmail.com>
2020-12-19 18:31:33 +0530

    prod env 2

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "test") {
   require("dotenv").config({ path: ".env.test" });
 } else if (process.env.NODE_ENV === "development") {
   require("dotenv").config({ path: ".env.development" });
-} else {
+} else if (process.env.NODE_ENV === "production") {
   require("dotenv").config({ path: ".env" });
 }
 
