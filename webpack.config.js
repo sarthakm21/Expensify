88a05ab6cb7b11c36d7c36398b6871675d388d44
sarthakm21 <sarthakmaheshwari140@gmail.com>
2020-12-19 18:40:02 +0530

    idk

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,6 @@ if (process.env.NODE_ENV === "test") {
   require("dotenv").config({ path: ".env.test" });
 } else if (process.env.NODE_ENV === "development") {
   require("dotenv").config({ path: ".env.development" });
-} else if (process.env.NODE_ENV === "production") {
-  require("dotenv").config({ path: ".env" });
 }
 
 module.exports = (env) => {
