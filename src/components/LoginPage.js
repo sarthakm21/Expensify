5b9ff24e5b9a957752f2f3a5a30e8f958b0e2bc9
sarthakm21 <sarthakmaheshwari140@gmail.com>
2021-04-26 18:11:37 +0530

    login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,13 +4,18 @@ import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => {
   return (
-    <div className="box-layout">
-      <div className="box-layout__box">
-        <h1 className="box-layout__title">Expensify</h1>
-        <p>Get your expenses on track</p>
-        <button onClick={startLogin} className="button">
-          Login with Google
-        </button>
+    <div className="login-container">
+      <div className="box-layout">
+        <div className="box-layout__box">
+          <h1 className="box-layout__title">Expensify</h1>
+          <p>Get your expenses on track</p>
+          <button onClick={startLogin} className="button">
+            Login with Google
+          </button>
+        </div>
+      </div>
+      <div className="login-background">
+        <img src="images/wallet.svg" alt="bg" />
       </div>
     </div>
   );
